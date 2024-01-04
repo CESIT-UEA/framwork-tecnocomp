69dd616eacef9ccfc58b20462b2631175ba68756
HenriqueGalvim <henrixe80@gmail.com>
2024-01-04 11:28:27 -0400

    Ate aq, foi refatorado partes do codigo de reatividade do mecanismo de ir para atividade, ao inves de um ngIf foi alterado pra um ngSwitch Case, ele encaixa melhor na situacao e permite flexibilidade maior, entretanto, um ponto a se fazer futuramente eh juntar estilos css e otimizar pois eles estao ficando mt verbosos, eh ineficiente. Ate aqui foi feito a parte da atividade, do saiba mais, das referencias. A maior parte da estilizacao foi feita, conforme a prototipacao.

--- src/app/pages/modulos/components/behaviorismo/behaviorismo.component.ts
+++ src/app/pages/modulos/components/behaviorismo/behaviorismo.component.ts
@@ -7,13 +7,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./behaviorismo.component.css']
 })
 export class BehaviorismoComponent {
+  arrayReferencias:string[] = [
+    'BOSI, Alfredo. Historia concisa da literatura brasileira. 38. ed. São Paulo: Cortes, 2002',
+    'BOSI, Alfredo. Historia concisa da literatura brasileira. 38. ed. São Paulo: Cortes, 2002',
+    'BOSI, Alfredo. Historia concisa da literatura brasileira. 38. ed. São Paulo: Cortes, 2002',
+  ]
+  arrayLinks:string[] = [
+    'https://br.search.yahoo.com/search?fr=mcafee&type=E210BR91199G0&p=saiba+mais+uea','https://br.search.yahoo.com/search?fr=mcafee&type=E210BR91199G0&p=saiba+mais+uea',
+    'https://br.search.yahoo.com/search?fr=mcafee&type=E210BR91199G0&p=saiba+mais+uea','https://br.search.yahoo.com/search?fr=mcafee&type=E210BR91199G0&p=saiba+mais+uea'
+  ]
+
   arrayVideos:string[] = ['https://www.youtube.com/embed/c6kVJNkAeH4?si=78pwIHKZf_PDQuW_','https://www.youtube.com/embed/Vh8oOY0v9bg?si=_9t9bhiKjef2VgWK'];
   pergunta: string = "Aprendemos na aula anterior que existem três passos principais para definir uma boa estratégia. Quais são eles?"
+
   arrayAlternativas = [
     {
       nome: 'A',
       descricao:'Lorem ipsum dolor sit amet consectetur. Erat quis luctus sed semper volutpat congue turpis.',
-      correto:false
+      correto:true
     },
     {
       nome: 'B',
@@ -31,12 +42,18 @@ export class BehaviorismoComponent {
       correto:false
     },
   ]
-
-  isAtividadeTrue:boolean = true
+  controllerSwitch:number = 0
 
   atividadeClick(){
-    this.isAtividadeTrue = this.isAtividadeTrue == true ? false : true
-    console.log(this.isAtividadeTrue)
+    this.controllerSwitch = this.controllerSwitch == 0 ? 1 : 0
+  }
+
+  referenciasClick(){
+    this.controllerSwitch = this.controllerSwitch == 0 ? 2 : 0
+  }
+
+  linksClick(){
+    this.controllerSwitch = this.controllerSwitch == 0 ? 3 : 0
   }
 
 }
