7abf628e15828be0cf3e8a2a6862158db5390523
HenriqueGalvim <henrixe80@gmail.com>
2024-02-22 19:02:36 -0400

    Foram feitas alterações no componente saiba mais, foi em grande parte concluído. Faltando apenas alguns pequenos ajustes. (Kauê na máquina do Galvim)

--- src/app/pages/modulos/components/saiba-mais/saiba-mais.component.ts
+++ src/app/pages/modulos/components/saiba-mais/saiba-mais.component.ts
@@ -8,4 +8,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class SaibaMaisComponent {
   @Input() links!: string[];
   @Output() linksClick = new EventEmitter<void>();
+
+  saibaMais: any = [
+  {
+    descricao: 'O que é Behaviorismo? Entenda um pouco da Psicologia Comportamental',
+    url: 'https://br.search.yahoo.com/search?fr=mcafee&type=E210BR714G91805&p=interroga%C3%A7%C3%A3o'
+   },
+   {
+    descricao: 'Behaviorismo - o ser humano é realmente livre?',
+    url: 'https://br.search.yahoo.com/search?fr=mcafee&type=E210BR714G91805&p=interroga%C3%A7%C3%A3o'
+   },
+   {
+    descricao: 'O que é Behaviorismo? Entenda um pouco da Psicologia Comportamental',
+    url: 'https://br.search.yahoo.com/search?fr=mcafee&type=E210BR714G91805&p=interroga%C3%A7%C3%A3o'
+   }
+  ]
+
 }
