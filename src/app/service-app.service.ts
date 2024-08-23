741cbc699d72545576e06b24041da18b6dfd0d5a
Henrique <henrixe80@gmail.com>
2024-08-23 16:25:09 -0400

    Mais uma vez

--- src/app/service-app.service.ts
+++ src/app/service-app.service.ts
@@ -106,7 +106,7 @@ export class ServiceAppService {
       Authorization: 'Bearer ' + this.tokenStorage,
     });
 
-    return this.http.post(this.apiUrl + '/gradeIn' + this.tokenStorage, grade, {
+    return this.http.post(this.apiUrl + '/gradeIn', grade, {
       headers: headers,
     });
   }
